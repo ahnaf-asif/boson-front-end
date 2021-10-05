e607753812f5c2955180c3c997059de389f9329b
asifthen00b <ahnafshahriar92@gmail.com>
2021-10-05 11:39:33 +0600

    added stuff

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,8 +21,35 @@
       <MobileNavLinks></MobileNavLinks>
     </v-navigation-drawer>
 
-    <v-footer :absolute="!fixed" app >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer :absolute="!fixed" app color="blue-grey darken-4 white--text" class="py-10">
+      <v-row> 
+        <v-col cols="12" md="5">
+          <h1 class="mb-10">Let's work Together</h1>
+          <p style="max-width: 50ch;">Our club is open for School, College and University going students all over the country. We work on different olympiads, Scientific Magazine, publishing blogs and many other things. We organize Mega festivals, various Olympiads and competitions. We also organize camps and seminars.</p>
+          <p>Please Join us Here if you haven't already.</p>
+        </v-col>
+        <v-col cols="12" md="3">
+          <h1 class="mb-10 text-center">Quick Links</h1>
+          <v-row>
+            <v-col cols="6" style="">
+              <ul>
+                <li>List Item 1</li>
+              </ul>
+            </v-col>
+            <v-col cols="6" style="text-align:right;">
+              <ul class="" >
+                <li>List item 2</li>
+              </ul>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col>
+          <h1 class="text-center mb-10">Contact Us</h1>
+          <p class="text-center">+880 1633923851</p>
+          <p class="text-center">bosonbiggansangho@gmail.com</p>
+          <p class="text-center">Leave us a message here.</p>
+        </v-col>
+      </v-row>
     </v-footer>
   </v-app>
 </template>
@@ -49,6 +76,9 @@
 }
 .navbar-v-menu{
   border: 2px solid darkorange !important;
+}
+ul{
+  list-style: none;
 }
 </style>
 
